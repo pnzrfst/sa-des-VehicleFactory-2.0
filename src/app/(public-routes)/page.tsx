a39674b22f4abfced73b64a0a38c991fb76cfd47
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2025-03-31 21:55:35 -0300

    Feat: Criando a página de login

--- src/app/(public-routes)/page.tsx
+++ src/app/(public-routes)/page.tsx
@@ -1,5 +1,35 @@
+import Button from '@/components/Button';
+import './styles.css'
+
 export default function Home() {
   return (
-    <h1>Login</h1>
+    <div className='main'>
+
+      <span className='lateral-login'>
+        <div className='container'>
+          <h1>Bem-vindo de volta!</h1>
+          <h2 className='text-container'>Para continuar, por favor, acesse sua conta.</h2>
+          <h2 className='text-container'>Não possui conta? </h2>
+        </div>
+        <br />
+        <Button src='Cadastre-se' dataType='register-button' />
+      </span>
+      <div className='div-form'>
+        <form className='login'>
+
+          <label htmlFor="email">Email</label>
+          <input type="text" id='email' />
+
+          <label htmlFor="password">Senha</label>
+          <input type="text" id='password' />
+
+          <Button src='Login' dataType='basic-button' />
+        </form>
+      </div>
+
+
+    </div>
+
+
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,12 @@
-export function Button() {
-    return <h1>BUTTON</h1>
+import './styles.css'
+
+type buttonProps ={
+    src: string;
+    dataType:'register-button' | 'basic-button' | 'browser-btton'
+}
+
+export default function Button({src,dataType}:buttonProps) {
+    return (
+        <button  className={dataType}>{src}</button>
+    )
 }
