690f4e47b01c934c9890c632463e1ad2b49ffbe9
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2025-03-31 21:55:19 -0300

    Feat: Criando o módulo da pagina principal

--- src/app/(private-route)/layout.tsx
+++ src/app/(private-route)/layout.tsx
@@ -1,7 +1,11 @@
+import LateralMenu from "@/components/LateralMenu"
+import './styles.css'
+
 export default function PrivateLayout({ children }: { children: React.ReactNode}){
     return (
-        <>
+        <div className="layout">
+            <LateralMenu></LateralMenu>
             {children}
-        </>
+        </div>
     )
 }
