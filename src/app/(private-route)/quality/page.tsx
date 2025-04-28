f796c704c5beb7cd5d6f6954a28118bac03f2bdd
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2025-04-28 19:17:48 -0300

    FEAT: SUBINDO A BRANCH DE QUALIDADE

---
+++ src/app/(private-route)/quality/page.tsx
@@ -0,0 +1,11 @@
+import MainPage from "@/components/MainPage";
+
+export default function Quality() {
+    return (
+        <div>
+            <MainPage namePage="Qualidade" moduloPage="Veículos" header={[""]}></MainPage>
+
+            
+        </div>
+    )
+}
