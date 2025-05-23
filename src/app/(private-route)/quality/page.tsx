8d1386fceab13af2646139d0f6eb13fba7d3155b
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2025-05-22 21:52:38 -0300

    quality

--- src/app/(private-route)/quality/page.tsx
+++ src/app/(private-route)/quality/page.tsx
@@ -1,11 +1,88 @@
-import MainPage from "@/components/MainPage";
+import Button from '@/components/Button';
+import './styles.css'
+import { FaCheck } from "react-icons/fa6";
+import { MdClose } from "react-icons/md";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoCheckmarkDoneSharp  } from "react-icons/io5";
+import { TbClockHour4Filled } from "react-icons/tb";
 
 export default function Quality() {
     return (
-        <div>
-            <MainPage namePage="Qualidade" moduloPage="Veículos" header={[""]}></MainPage>
+        <section className="body-container">
 
-            
-        </div>
+            <div className="home-infos">
+                <header>
+                    <h1>Qualidade & Inspeções</h1>
+                    <span></span>
+                    <h3>Principal</h3>
+                </header>
+                <main className='main'>
+                    <div className='filter'>
+                        <button className='pending-button'><TbClockHour4Filled size={20}/>
+                        Pendentes</button>
+                        <button className='approved-button'><IoCheckmarkDoneSharp size={20} />Aprovados</button>
+                        <button className='repproved-button'><MdClose size={20}/>Reprovados</button>
+                    </div>
+                    <div className="dashboard">
+
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                        <div className='card'>
+                            <div className='item'>
+                                <h1>Item na qualidade</h1>
+                            </div>
+                            <div className='buttons-area'>
+                                <button className='approved'>Aprovado<FaCheck size={20} /></button>
+                                <button className='repproved'>Reprovado <IoCloseCircleOutline size={20} /></button>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </section>
+
     )
 }
--- src/components/ModalRegister/index.tsx
+++ src/components/ModalRegister/index.tsx
@@ -1,36 +1,68 @@
-import { Dialog, TextField } from "@mui/material";
-import { Close } from "@mui/icons-material";
+'use client'
+import { useState } from "react";
+import { Dialog, TextField, InputAdornment, IconButton } from "@mui/material";
+import { Close, Visibility, VisibilityOff } from "@mui/icons-material";
 import './styles.css'
 import Button from "../Button";
-
 
 type ModalRegisterProps = {
     open: boolean
     handleClose: (value: boolean) => void
 }
 
-export function ModalRegister({open, handleClose}: ModalRegisterProps){
+export function ModalRegister({ open, handleClose }: ModalRegisterProps) {
+    const [mostrarSenha, setMostrarSenha] = useState(false);
+    const [mostrarConfirmarSenha, setMostrarConfirmarSenha] = useState(false);
 
-    return(
-        <Dialog
-            open = {open}
-            onClose={handleClose}
-        >
-            <div className="close" onClick={()=> handleClose(false)}>
+    return (
+        <Dialog open={open} onClose={() => handleClose(false)}>
+            <div className="close" onClick={() => handleClose(false)}>
                 <Close />
             </div>
             <div className="container">
-                <h1>Cadastar Conta</h1>
-                <TextField label="Nome" variant="outlined" />
-                <TextField label="Email" variant="outlined" />
-                <TextField label="Data Nascimento" variant="outlined" />
-                <TextField label="Senha" variant="outlined" />
-                <TextField label="Confirmar Senha" variant="outlined" />
+                <h1>Cadastrar Conta</h1>
+                <TextField label="Nome" variant="outlined" fullWidth margin="dense" />
+                <TextField label="Email" variant="outlined" fullWidth margin="dense" />
+                <TextField label="Data Nascimento" variant="outlined" fullWidth margin="dense" />
+                
+                <TextField
+                    label="Senha"
+                    variant="outlined"
+                    type={mostrarSenha ? "text" : "password"}
+                    fullWidth
+                    margin="dense"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => setMostrarSenha(!mostrarSenha)} edge="end">
+                                    {mostrarSenha ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
+
+                <TextField
+                    label="Confirmar Senha"
+                    variant="outlined"
+                    type={mostrarConfirmarSenha ? "text" : "password"}
+                    fullWidth
+                    margin="dense"
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => setMostrarConfirmarSenha(!mostrarConfirmarSenha)} edge="end">
+                                    {mostrarConfirmarSenha ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
+
                 <div className="buttonGroup">
-                    <Button src="Cadastre-se" dataType="basic-button"/>
+                    <Button src="Cadastre-se" dataType="basic-button" />
                 </div>
             </div>
-
         </Dialog>
-    )
+    );
 }
