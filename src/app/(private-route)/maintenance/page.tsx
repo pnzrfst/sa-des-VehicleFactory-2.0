0dd77e3f1c6fab1db848e3fc7e40444213db0e86
pnzrfst <jxssxp@gmail.com>
2025-06-02 19:11:33 -0300

    subindo

--- src/app/(private-route)/maintenance/page.tsx
+++ src/app/(private-route)/maintenance/page.tsx
@@ -1,7 +1,54 @@
 
 import MainPage from "@/components/MainPage";
+import {API} from '@/api'
+import { useEffect, useState } from "react";
+
+interface Maintenance {
+    id: string;
+    description: string;
+    idProduction: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface UlItem {
+    li1: string;
+    li2: string;
+    li3: string;
+    li4: string;
+}
+
 
 export default function Maintenance() {
+    const [maintenances, setMaintenance] = useState<Maintenance[]>([])
+    const [ulItens, setUlItens] = useState<UlItem[]>([])
+
+    useEffect(() => {
+        loadMaintenances()
+    }, [])
+    
+    async function loadMaintenances() {
+        try {
+            const response = await API.get<Maintenance[]>("/maintenance");
+            setMaintenance(response.data);
+
+            const itensFormatados : UlItem = response.data.map((item : Maintenance) => ({
+                li1: item.description,
+                li2: item.idProduction,
+                li3: item.createdAt,
+                li4: item.updatedAt
+            }))
+
+
+            setUlItens(itensFormatados)
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return(
         <MainPage namePage="Manutenção" 
         moduloPage="Veículos" 
