5ccce91e342201ee3d7a9191cc2c65ec3fd49b8f
pnzrfst <jxssxp@gmail.com>
2025-06-04 17:53:33 -0300

    subindo

--- src/app/(private-route)/production/page.tsx
+++ src/app/(private-route)/production/page.tsx
@@ -1,15 +1,42 @@
+"use client";
+import { API } from "@/api";
+import MainPage from "@/components/MainPage";
+import { useEffect, useState } from "react";
 
-import MainPage from "@/components/MainPage"
+interface Production {
+  vehicleProduced: string;
+  quantity: number;
+  dateStart: Date;
+  endDate: Date;
+  id: string;
+}
 
+export default function Production() {
+  const [production, setProduction] = useState<number>(0);
 
+  async function getNumberOfProductions() {
+    try {
+      const response = await API.get<Production[]>("/production");
+      const nextProduction = response.data.length + 1;
+      setProduction(nextProduction);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-export default function Production(){
+  useEffect(() => {
+    getNumberOfProductions();
+  }, []);
 
-    
-    return (
-        <section className="body">
-            <MainPage namePage="Produção" moduloPage="Armazém Principal" header={[""]} isProduction = {true} numberProduction={1}></MainPage>
-
-        </section>
-    )
+  return (
+    <section className="body">
+      <MainPage
+        namePage="Produção"
+        moduloPage="Armazém Principal"
+        header={[""]}
+        isProduction={true}
+        numberProduction={production}
+      ></MainPage>
+    </section>
+  );
 }
